26b4785d588c374fc42269450dfdfeacb279c82f
Aksaannajose <77113062+Aksaannajose@users.noreply.github.com>
2025-01-10 14:37:04 +0530

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using static StudentEnrollmentSystem.Models;
+using System.Security.Cryptography;
+
+namespace StudentEnrollmentSystem
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+          : base(options)
+        {
+        }
+
+       
+
+
+        public DbSet<Stud> Stud { get; set; }
+        public DbSet<Courses> Courses { get; set; }
+        public DbSet<EnrollmentCourse> EnrollmentCourses { get; set; }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
